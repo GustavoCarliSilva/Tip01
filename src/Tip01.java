4d36668fa95cb10b667d4ba69d6281abdc2120cd
ALUNO <ALUNO@LAB04-PC07>
2024-06-13 21:29:18 -0300

    Commit Project Tip01

---
+++ src/Tip01.java
@@ -0,0 +1,52 @@
+public class Tip01 {
+    public static void main(String[] args) {
+        //Find everyone's indivudlal total after tax(5%) and tip(15%)
+        
+        
+        double Person1 = 10;
+        double Person2 = 12;
+        double Person3 = 9;
+        double Person4 = 8;
+        double Person5 = 7;
+        double Person6 = 15;
+        double Person7 = 11;
+        double Person8 = 30;
+        
+        double Total1 = Person1*(1 +.05 +.15);
+        System.out.println(Total1);
+        
+        double Total2 = Person2*(1 +.05 +.15);
+        System.out.println(Total2);
+        
+        double Total3 = Person3*(1 +.05 +.15);
+        System.out.println(Total3);
+        
+        double Total4 = Person4*(1 +.05 +.15);
+        System.out.println(Total4);
+        
+        double Total5 = Person5*(1 +.05 +.15);
+        System.out.println(Total5);
+        
+        double Total6 = Person6*(1 +.05 +.15);
+        System.out.println(Total6);
+        
+        double Total7 = Person7*(1 +.05 +.15);
+        System.out.println(Total7);
+        
+        double Total8 = Person8*(1 +.05 +.15);
+        System.out.println(Total8);
+        
+        
+        
+        
+       
+        
+        
+        
+        
+        
+        
+        
+        
+    }    
+}
